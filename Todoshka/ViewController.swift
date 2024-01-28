ea5553f4f5e797797c90efb8c8e99e249ea69f73
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-01-28 00:56:54 -0500

    Initial Commit

---
+++ Todoshka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoshka
+//
+//  Created by Timur on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
