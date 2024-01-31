c28f03e141474ff2b75e04c0cd24f5cb6774bc02
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-01-30 23:47:08 -0500

    Persistent Local Data Storage was added

--- Todoshka/ToDoListViewController.swift
+++ Todoshka/ToDoListViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Kill Bill"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     
@@ -59,6 +64,9 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
